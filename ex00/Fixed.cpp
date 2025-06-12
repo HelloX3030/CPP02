c371f6a2c315d22d8b9588d2e644e132118218b9
lseeger <lseeger@student.42.fr>
2025-06-12 16:46:21 +0200

    fixed some stuff

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -3,19 +3,23 @@
 Fixed::Fixed()
     : raw_bits(0)
 {
+    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
+    std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &other)
 {
+    std::cout << "Copy constructor called" << std::endl;
     raw_bits = other.raw_bits;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
+    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other) {
         raw_bits = other.raw_bits;
     }
@@ -24,10 +28,12 @@ Fixed &Fixed::operator=(const Fixed &other)
 
 int Fixed::getRawBits( void) const
 {
+    std::cout << "getRawBits member function called" << std::endl;
     return (raw_bits);
 }
 
 void Fixed::setRawBits( int const raw )
 {
+    std::cout << "setRawBits member function called" << std::endl;
     raw_bits = raw;
 }
